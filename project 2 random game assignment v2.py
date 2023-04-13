531857c2080d50883980e2de21ed5ed62304abee
Ault <ault@andrims.com>
2023-04-13 17:02:55 -0400

    v2 final

--- project 2 random game assignment v2.py
+++ project 2 random game assignment v2.py
@@ -25,11 +25,15 @@ def calculate_hand_value(hand):
     return value
 
 # Define a function to display the hands and scores
-def display_game(player_hand, ai_hand, player_score, ai_score):
+def display_game(player_hand, ai_hand, player_score, ai_score, show_ai_hand):
     print("Player's hand:", player_hand)
     print("Player's score:", player_score)
-    print(ai_name + "'s hand:", [ai_hand[0], "X"])
-    print(ai_name + "'s score: ?", "\n")
+    if show_ai_hand:
+        print(ai_name + "'s hand:", ai_hand)
+        print(ai_name + "'s score:", ai_score)
+    else:
+        print(ai_name + "'s hand:", [ai_hand[0], "X"])
+        print(ai_name + "'s score: ?", "\n")
 
 # Start the game loop
 while True:
@@ -41,7 +45,7 @@ while True:
     # Display the initial hands and scores
     player_score = calculate_hand_value(player_hand)
     ai_score = calculate_hand_value([ai_hand[0]])
-    display_game(player_hand, ai_hand, player_score, ai_score)
+    display_game(player_hand, ai_hand, player_score, ai_score, False)
 
     # Player's turn
     while True:
@@ -49,7 +53,7 @@ while True:
         if choice.lower() == 'hit':
             player_hand.append(deck.pop())
             player_score = calculate_hand_value(player_hand)
-            display_game(player_hand, ai_hand, player_score, ai_score)
+            display_game(player_hand, ai_hand, player_score, ai_score, False)
             if player_score > 21:
                 print("Player busts!")
                 scores['ai'] += 1
@@ -62,7 +66,7 @@ while True:
         while ai_score < 17:
             ai_hand.append(deck.pop())
             ai_score = calculate_hand_value(ai_hand)
-            display_game(player_hand, ai_hand, player_score, ai_score)
+            display_game(player_hand, ai_hand, player_score, ai_score, True)
             time.sleep(1)
         if ai_score > 21:
             print(ai_name + " busts!")

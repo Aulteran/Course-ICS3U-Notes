e960fd39070ba45d21cd3be2d94343306266c95d
Aadil Hussain <ault@andrims.com>
2023-04-14 17:14:21 +0000

    added 2nd item in gradebook support

--- project 3 - teacher gradebook assign.py
+++ project 3 - teacher gradebook assign.py
@@ -9,38 +9,51 @@
 # 7) Have the program calculate the mean, median, and mode of the entire class
 # University LEVEL ADD-ON:
 # 8) Have the program able to call and display a student grade’s by inputting their name
+# University - Must have two assignments
 
 import statistics
 
 gradebook = {}
 
-all_grades_list = []
+assign1_grades_list = []
+assign2_grades_list = []
+
 try:
     num_students = int(input("how many students in gradebook: "))
-    assign_val = int(input('what is the assigment out of: '))
+    assign1val = int(input('value for assignment 1: '))
+    assign2val = int(input('value for assignment 2: '))
 except(ValueError):
     print("invalid input, restart")
 
 for i in range(1, num_students+1):
     try:
-        gradebook[input(f"Name for student{i}: ")]['Mark'] = int(input(f"Mark for student{i}: "))
+        gradebook[input(f"Name for student{i}: ")] = {}
     except(ValueError):
         print("invalid input")
 
+
 for student in gradebook:
-    gradebook[student]['Percent Grade'] = (gradebook[student]['Mark'] / assign_val) * 100
-    all_grades_list.append(gradebook[student]['Percent Grade'])
+    gradebook[student]['Assign1']['Mark'] = int(input("Mark for assignment 1: "))
+    gradebook[student]['Assign2']['Mark'] = int(input("Mark for assignment 2: "))
+    gradebook[student]['Assign1']['Percent Grade'] = (gradebook[student]['Assign1']['Mark'] / assign1val) * 100
+    gradebook[student]['Assign2']['Percent Grade'] = (gradebook[student]['Assign2']['Mark'] / assign2val) * 100
+    assign1_grades_list.append(gradebook[student]['Assign1']['Percent Grade'])
+    assign1_grades_list.append(gradebook[student]['Assign2']['Percent Grade'])
 
 while True:
     do_sum = int(input("===GRADEBOOK===\n1. View Class Stats\n2. View Student\n3. Exit\nSelect: "))
     if do_sum == 1:
-        print('Class Mean: ', statistics.mean(all_grades_list))
-        print('Class Mode: ', statistics.mode(all_grades_list))
-        print('Class Median: ', statistics.median(all_grades_list))
+        print('Class Assign1 Mean: ', statistics.mean(assign1_grades_list))
+        print('Class Assign1 Mode: ', statistics.mode(assign1_grades_list))
+        print('Class Assign1 Median: ', statistics.median(assign1_grades_list))
+        print('Class Assign2 Mean: ', statistics.mean(assign2_grades_list))
+        print('Class Assign2 Mode: ', statistics.mode(assign2_grades_list))
+        print('Class Assign2 Median: ', statistics.median(assign2_grades_list))
     elif do_sum == 2:
         lookup = input("Student to lookup: ")
-        print('Mark for Student: ', gradebook[lookup]['Mark'])
-        print('Grade for Student: ', gradebook[lookup]['Percent Grade'])
+        assignlookup = input("assignment to lookup: ")
+        print('Mark for Student: ', gradebook[assignlookup][lookup]['Mark'])
+        print('Grade for Student: ', gradebook[assignlookup][lookup]['Percent Grade'], '%')
     elif do_sum == 3:
         break
     else:

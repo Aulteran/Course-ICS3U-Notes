6705d5f87f89d19fed4bbf250786ea93e28bdfad
Ault <ault@andrims.com>
2023-04-18 23:31:54 -0400

    April 18 csv challenge instructions

---
+++ April18 .csv files challenge.py
@@ -0,0 +1,16 @@
+# A software company wished for you to databse the
+# personal emails of all of its employees. Create a program that:
+
+# Func1:
+# Asks the user for their email
+# Stores the email to a txt file
+
+# Func2:
+# Removes a selected email
+# Resaves the new list
+
+def add_email():
+    raise NotImplementedError
+
+def remove_email():
+    raise NotImplementedError

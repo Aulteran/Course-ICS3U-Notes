75641cd4189da0f401c9ce1f3eda51617647acd8
Aadil Hussain <ault@andrims.com>
2023-04-28 13:25:45 -0400

    Added Year Released

--- Quiz-PartB.py
+++ Quiz-PartB.py
@@ -26,32 +26,34 @@ def get_num(prompt):
     except(ValueError):
         print("invalid input")
 
-def new_entry(movie_name, director, gross_sales):
+def new_entry(movie_name, year, director, gross_sales):
     '''creates a new entry in movies db'''
-    movies[movie_name] = [director, gross_sales]
+    movies[movie_name] = [year, director, gross_sales]
 
 def call_entry(movie_name):
     '''shows details for a specific movie'''
     for movie, moviedata in movies.items():
         if movie == movie_name:
-            print(f"Movie Name: {movie}\nDirector: {moviedata[0]}\nGross Sales: {moviedata[1]}")
+            print(f"Movie Name: {movie}\nYear: {moviedata[0]}\nDirector: {moviedata[1]}\nGross Sales: {moviedata[2]}")
             return
     print("Entry not found.")
 
-def change_entry(movie_name, new_director, new_sales):
+def change_entry(movie_name, new_year, new_director, new_sales):
     '''changes movie item in db'''
     for movie, moviedata in movies.items():
         if movie == movie_name:
-            moviedata[0] = new_director
-            moviedata[1] = new_sales
+            moviedata[0] = new_year
+            moviedata[1] = new_director
+            moviedata[2] = new_sales
 
 while True:
     menu_select = get_num(menu)
     if menu_select == 1:
         name = input("what is the movie's name: ").upper()
+        year = input("What year did the movie release: ")
         director = input("what is the movie's director: ").upper()
         sales = input("what is the movie's gross sales: ").upper()
-        new_entry(name, director, sales)
+        new_entry(name, year, director, sales)
         print("Entry Created.")
     elif menu_select == 2:
         name = input("What movie's details would you like to view: ").upper()
@@ -62,9 +64,10 @@ while True:
         if proceed_query[0] == 'N':
             continue
         name = input("Name of movie to modify: ").upper()
+        year = input("New movie release year: ")
         director = input("new movie director: ").upper()
         sales = input("new movie gross sales: ").upper()
-        change_entry(name, director, sales)
+        change_entry(name, year, director, sales)
     elif menu_select == 4:
         break
     else:
